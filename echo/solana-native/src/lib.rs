64577987c9e4e3d1bf97b88a3be91492a1cec5d4
ngundotra <noah@gundotra.org>
2022-09-06 21:16:05 -0400

    add memo instruction to native

--- echo/solana-native/src/lib.rs
+++ echo/solana-native/src/lib.rs
@@ -4,8 +4,11 @@ use solana_program::{
     entrypoint::ProgramResult,
     instruction::Instruction,
     msg,
+    program::invoke,
+    program_error::ProgramError,
     pubkey::Pubkey,
 };
+use spl_memo::build_memo;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
@@ -20,6 +23,27 @@ pub fn process_instruction(
     accounts: &[AccountInfo],
     instruction_data: &[u8],
 ) -> ProgramResult {
+    let (instruction_discriminant, instruction_data_inner) = instruction_data.split_at(1);
+    match instruction_discriminant[0] {
+        0 => {
+            msg!("Instruction: Echo Log");
+            process_echo_log(accounts, instruction_data_inner)?;
+        }
+        1 => {
+            msg!("Instruction: Echo Memo");
+            process_echo_memo(accounts, instruction_data_inner)?;
+        }
+        _ => {
+            msg!("Error: unknown instruction")
+        }
+    }
+    Ok(())
+}
+
+pub fn process_echo_log(
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> Result<(), ProgramError> {
     // This is the standard way of accessing accounts passed to an on-chain program
     let account_info_iter = &mut accounts.iter();
 
@@ -30,7 +54,34 @@ pub fn process_instruction(
     // Log the pubkey that signed this instruction and message
     // It will have format like:
     // Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS: Hello world
-    msg!("{}", format!("{:?}: {}", signer.key, message));
+    msg!(
+        "{}",
+        format!(
+            "{:?}: {}",
+            signer.key,
+            std::str::from_utf8(&instruction_data).unwrap()
+        )
+    );
+    Ok(())
+}
+
+pub fn process_echo_memo(
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> Result<(), ProgramError> {
+    // This is the standard way of accessing accounts passed to an on-chain program
+    let account_info_iter = &mut accounts.iter();
+
+    // There is only 1 instruction, and we assume that the first
+    // account is meant to be the signer
+    let signer = next_account_info(account_info_iter)?;
+
+    // Log the pubkey that signed this instruction and message
+    // It will have format like:
+    // Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS: Hello world
+
+    let memo_instruction = build_memo(instruction_data, &[&signer.key]);
+    invoke(&memo_instruction, &[signer.clone()])?;
     Ok(())
 }
 
--- echo/solana-native/tests/echo.test.ts
+++ echo/solana-native/tests/echo.test.ts
@@ -5,14 +5,16 @@ import {
     Transaction,
     TransactionInstruction,
     PublicKey,
-    TransactionSignature,
     sendAndConfirmTransaction,
     LAMPORTS_PER_SOL,
-    MessageV0
+    VersionedTransactionResponse,
+    MessageCompiledInstruction
 } from "@solana/web3.js";
+import * as bs58 from 'bs58';
 import { assert } from "chai";
 
 const PROGRAM_ID = new PublicKey("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+const MEMO_PROGRAM_ID = new PublicKey("MemoSq4gqABAXKb96qnH8TysNcWxMyWCqXgDLGmfcHr");
 
 type EchoInstructionAccounts = {
     signer: Signer
@@ -21,7 +23,7 @@ type EchoInstructionArgs = {
     message: String
 }
 
-function createEchoInstruction(
+function createEchoLogInstruction(
     accounts: EchoInstructionAccounts,
     args: EchoInstructionArgs
 ): TransactionInstruction {
@@ -34,7 +36,29 @@ function createEchoInstruction(
                 isWritable: false,
             }
         ],
-        data: Buffer.from(args.message, 'utf-8'),
+        data: Buffer.concat([Buffer.from([0x0]), Buffer.from(args.message, 'utf-8')]),
+    };
+}
+
+function createEchoMemoInstruction(
+    accounts: EchoInstructionAccounts,
+    args: EchoInstructionArgs
+): TransactionInstruction {
+    return {
+        programId: PROGRAM_ID,
+        keys: [
+            {
+                pubkey: accounts.signer.publicKey,
+                isSigner: true,
+                isWritable: false,
+            },
+            {
+                pubkey: MEMO_PROGRAM_ID,
+                isSigner: false,
+                isWritable: false
+            }
+        ],
+        data: Buffer.concat([Buffer.from([0x1]), Buffer.from(args.message, 'utf-8')]),
     };
 }
 
@@ -52,6 +76,30 @@ function parseMessage(logMessages: string[], publicKey: PublicKey): string {
     throw Error("Could not find our message in the transaction log messages");
 }
 
+function parseMessageMemo(txInfo: VersionedTransactionResponse, publicKey: PublicKey): string {
+    const accountKeys = txInfo.transaction.message.staticAccountKeys;
+    const getDecompiledPublicKey = (index: number): PublicKey => accountKeys[index];
+    const topLevelIxs = txInfo.transaction.message.compiledInstructions;
+    const getCompiledIx = (index: number): MessageCompiledInstruction => topLevelIxs[index];
+
+    for (const topLevelIx of txInfo.meta?.innerInstructions ?? []) {
+        for (const innerIx of topLevelIx.instructions) {
+            if (getDecompiledPublicKey(innerIx.programIdIndex).equals(MEMO_PROGRAM_ID)) {
+                // Check that the calling program is our program, otherwise keep looking
+                if (!getDecompiledPublicKey(getCompiledIx(topLevelIx.index).programIdIndex).equals(PROGRAM_ID)) {
+                    continue;
+                }
+
+                // Decode the instruction data sent to the program
+                const innerIxData = bs58.decode(innerIx.data);
+                const decoder = new TextDecoder('utf-8');
+                return decoder.decode(innerIxData)
+            }
+        }
+    }
+    throw Error("Unable to find any echo memo instruction in the program")
+}
+
 describe("Echo Solana Native", () => {
     const connection = new Connection("http://localhost:8899");
 
@@ -60,7 +108,7 @@ describe("Echo Solana Native", () => {
     // Create a paper wallet (aka keypair), and send a transaction instruction
     // to our program. We test that the string we send as instruction data
     // can be retrieved from the log messages of the confirmed transaction.
-    it("Test our echo", async () => {
+    it.skip("Test echo using logs", async () => {
         // Create the paper wallet, and airdrop 1 SOL (does not work on mainnet-beta)
         const keypair = Keypair.generate();
         await connection.requestAirdrop(keypair.publicKey, LAMPORTS_PER_SOL);
@@ -69,7 +117,7 @@ describe("Echo Solana Native", () => {
         const message = "Hello World";
 
         // Create a TransactionInstruction to interact with our echo program
-        const ix: TransactionInstruction = createEchoInstruction({ signer: keypair }, { message });
+        const ix: TransactionInstruction = createEchoLogInstruction({ signer: keypair }, { message });
 
         // Put the instruction into a Transaction
         let tx = new Transaction().add(ix);
@@ -93,6 +141,41 @@ describe("Echo Solana Native", () => {
 
         // Let's look for our string with our paper wallet's pubkey
         const parsedMessage = parseMessage(txInfo!.meta!.logMessages!, keypair.publicKey);
+        assert(parsedMessage === message, "Logged string does not match passed string");
+    });
+    it("Test echo using spl-memo", async () => {
+        // Create the paper wallet, and airdrop 1 SOL (does not work on mainnet-beta)
+        const keypair = Keypair.generate();
+        await connection.requestAirdrop(keypair.publicKey, LAMPORTS_PER_SOL);
+
+        // We want this to be logged by the Solana runtime
+        const message = "Hello World";
+
+        // Create a TransactionInstruction to interact with our echo program
+        const ix: TransactionInstruction = createEchoMemoInstruction({ signer: keypair }, { message });
+
+        // Put the instruction into a Transaction
+        let tx = new Transaction().add(ix);
+
+        // Explicitly set the feePayer to be the signer
+        tx.feePayer = keypair.publicKey;
+        tx.recentBlockhash = (await connection.getLatestBlockhash('confirmed')).blockhash;
+
+        // Send transaction to network (local network)
+        const txId = await sendAndConfirmTransaction(connection, tx, [keypair], { skipPreflight: true, commitment: 'confirmed' });
+        // Retrieve transaction details
+        const txInfo = await connection.getTransaction(txId, {
+            commitment: 'confirmed',
+            maxSupportedTransactionVersion: 0,
+        });
+
+        // Check that the transaction succeeded
+        assert(!txInfo?.meta?.err, "Expected the instruction to succeed");
+        // Check that the transaction info has log messages
+        assert(txInfo?.meta?.logMessages, "Expected transaction to have logMessages");
+
+        // Let's look for our string with our paper wallet's pubkey
+        const parsedMessage = parseMessageMemo(txInfo!, keypair.publicKey);
         assert(parsedMessage === message, "Logged string does not match passed string");
     });
 });
